16131eb29bd049eff150a08bf68c86518035ba1c
gprime44 <guillaume.prime@gmail.com>
2016-07-06 17:23:09 +0200

    MAJ title movies

--- Contents/Code/updater.py
+++ Contents/Code/updater.py
@@ -3,6 +3,19 @@ GITHUB_REPOSITORY = 'gprime44/BitTorrent.bundle'
 LAST_VERSION = 'master'
 
 ################################################################################
+def get_latest_version():
+    try:
+        release_feed_url = 'https://github.com/{0}/releases.atom'.format(GITHUB_REPOSITORY)
+        release_feed_data = RSS.FeedFromURL(release_feed_url, cacheTime=0, timeout=5)
+        return release_feed_data.entries[0].title
+    except Exception as exception:
+        Log.Error('Checking for new releases failed: {0}'.format(repr(exception)))
+
+################################################################################
+def update_available():
+    return (True, None)
+
+################################################################################
 @route(SharedCodeService.common.PREFIX + '/update')
 def update():
     try:
